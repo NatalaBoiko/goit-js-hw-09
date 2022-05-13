c01e881058955d4053994b3e90dfba63fa13f056
NatalaBoiko <natalabojko@gmail.com>
2022-05-13 18:46:04 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const start = document.querySelector('[data-start]');
+console.log(start.textContent);
 
+const stop = document.querySelector('[data-stop]');
+console.log(stop.nodeName);
+const body = document.querySelector('body');
+// console.log(body.style);
+
+let timer = null;
+
+window.addEventListener('click', onStartBtnClick);
+window.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(event) {
+  if (event.target.textContent !== 'Start') {
+    return;
+  }
+  console.log('hello');
+  timer = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+}
+
+function onStopBtnClick(event) {
+  if (event.target.textContent !== 'Stop') {
+    return;
+  }
+  console.log('bye');
+  clearInterval(timer);
+  //   body.style.backgroundColor = '';
+}
