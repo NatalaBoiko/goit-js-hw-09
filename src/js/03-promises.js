94738feab07e069cb9b96638312ae10289b54ef9
NatalaBoiko <natalabojko@gmail.com>
2022-05-19 17:36:54 +0300

    03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('form');
+const delayEl = document.querySelector('[name="delay"]');
+const stepEl = document.querySelector('[name="step"]');
+const amountEl = document.querySelector('[name="amount"]');
+const btnEl = document.querySelector('button');
+
+formEl.addEventListener('submit', onBtnClick);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onBtnClick(evt) {
+  evt.preventDefault();
+  const delay = `${delayEl.value}`;
+  const delayStep = `${stepEl.value}`;
+  const amount = `${amountEl.value}`;
+  let newDelay = delay;
+
+  for (let position = 0; position < amount; position += 1) {
+    // newDelay += delayStep;
+    createPromise({ position, newDelay })
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    newDelay += delayStep;
   }
+  formEl.reset();
 }
